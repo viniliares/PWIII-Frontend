0295d8ecb29fb8d783d55a69fb75d6ed56540396
Your Name <you@example.com>
2019-11-22 20:27:10 -0300

    Exercicio 23

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaPagamentoComponent } from './lista-pagamento/lista-pagamento.component';
+import { NovoPagamentoComponent } from './novo-pagamento/novo-pagamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'listagem', component: ListaPagamentoComponent},
+  { path: 'novo', component: NovoPagamentoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
